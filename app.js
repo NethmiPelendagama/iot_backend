341b1b7778c7535d69019282eadb3645b768afb8
minosh00 <it20088682@my.sliit.lk>
2023-10-27 16:09:56 +0530

    psh

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose'); // Import mongoose
 const DataController = require('./controllers/dataController');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
+
+// Connect to MongoDB
+const DB_URL = "mongodb+srv://minosh:minosh@cluster0.u1yxx.mongodb.net/hotelssssssystem?retryWrites=true&w=majority";
+mongoose.connect(DB_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 const dataController = new DataController();
 
@@ -17,7 +25,7 @@ app.get('/data', (req, res) => {
   dataController.getAllData(req, res);
 });
 
-// Bind to all available network interfaces on port 3000
+// Bind to all available network interfaces on the specified port
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
